9b99cf865b8fccddda00a602c9679d899865b5ff
jeff.yang <jeff.yang@amlogic.com>
2010-12-10 14:35:15 +0800

    add insub(VOBSUB) show , add some ex idxsub parse file,make  a subtitle file invalid if it's line have more than 3000 words, add some jni interface

--- src/com/subtitleparser/subtypes/IdxSubParser.java
+++ src/com/subtitleparser/subtypes/IdxSubParser.java
@@ -1,5 +1,9 @@
 package com.subtitleparser.subtypes;
+import java.util.Arrays;
+
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.util.Log;
 
 import com.subtitleparser.MalformedSubException;
 import com.subtitleparser.SubData;
@@ -10,16 +14,35 @@ import com.subtitleparser.SubtitleParser;
 
 class IdxSubApi extends SubtitleApi
 {
+	native static RawData getIdxsubRawdata(int millisec); 
+	native void setIdxFile(String name);
 	 private Bitmap bitmap=null;
-	 IdxSubApi(){};
+	 private String filename;
+	 IdxSubApi(String name)
+	 {
+		 filename =name;
+		 setIdxFile(filename);
+	 };
 
 	 public SubData getdata(int millisec )
 	 {
 		 //add  value to bitmap
 		 //add  value to begingtime,endtime
-
-		
-		return new SubData( bitmap, begingtime,endtime);
+		 	getIdxsubRawdata(millisec);
+		    if(millisec%8000>4000)
+		    {
+			    Log.i("InSubApi", "------------getdata1-----------" );
+				int[] data = new int[100000];
+				Arrays.fill(data, 0x55555500);
+				bitmap= Bitmap.createBitmap( data,  50,  250, Config.ARGB_8888  ) ;
+		    }else
+		    {
+			    Log.i("InSubApi", "------------getdata2-----------" );
+				int[] data = new int[100000];
+				Arrays.fill(data, 0x99999900);    	
+				bitmap= Bitmap.createBitmap( data,  150,  180, Config.ARGB_8888  ) ;
+		    }
+			return new SubData( bitmap, millisec,millisec+3);
 	 };
 }
 
@@ -28,7 +51,7 @@ public class IdxSubParser implements SubtitleParser{
 	
 	public SubtitleApi parse(String filename) throws MalformedSubException{
 		//call jni to init parse;
-		return new IdxSubApi();
+		return new IdxSubApi(filename);
 	};
 
 	public SubtitleApi parse(String inputString,String st2) throws MalformedSubException{
