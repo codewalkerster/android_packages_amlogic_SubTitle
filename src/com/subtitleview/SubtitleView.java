08aaaed5b6c3898df7dacf56ffc8742dfec24f5f
xy.ding <xy.ding@amlogic.com>
2011-08-11 16:04:41 +0800

    fix sub show error

--- src/com/subtitleview/SubtitleView.java
+++ src/com/subtitleview/SubtitleView.java
@@ -7,306 +7,248 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.FrameLayout;
+import android.view.View;
+import android.view.Gravity;
 import com.subtitleparser.*;
 import android.graphics.Matrix;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.widget.LinearLayout;
 
-public class SubtitleView extends TextView {
-	private static final String LOG_TAG = SubtitleView.class.getSimpleName();
-	private SubtitleApi subapi = null;
-	private boolean needSubTitleShow= true;
-	private Subtitle.SUBTYPE type=Subtitle.SUBTYPE.SUB_INVALID;
-	private boolean InsubStatus=false;
-	private Bitmap inter_bitmap = null;
-	private Subtitle subtitle=null;
-	private int timeoffset=400;
-	private SubData data =null;
+public class SubtitleView extends FrameLayout {
+	private static final String TAG = SubtitleView.class.getSimpleName();
+	private boolean needSubTitleShow = true;
+	private int timeoffset = 400;
+	private SubData data = null;
 	private int graphicViewMode = 0;
-	private boolean hasopenInsubfile = false;
-	public void setGraphicSubViewMode(int flag)
-	{
-		graphicViewMode=flag;
+	private float wscale = 1.000f;
+	private float hscale = 1.000f;
+	private ImageView mImageView = null;
+	private TextView mTextView = null;
+	
+	public void setGraphicSubViewMode(int flag) {
+		graphicViewMode = flag;
 	}
 	
-
 	public SubtitleView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		init(context);
 	}
 
 	public SubtitleView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		subtitle = new Subtitle();
+		init(context);
 	}
 
 	public SubtitleView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		subtitle = new Subtitle();
-	}
-	public void setViewStatus(boolean flag )
-	{
-		needSubTitleShow=flag;
-		if(flag==false)
-		{
-			setVisibility( INVISIBLE); 
-		}
-		else
-		{
-			setVisibility( VISIBLE); 
-		}
+		init(context);
 	}
 	
-	
-	
-	
-	
-	public void tick(int millisec) {
-
-		if (needSubTitleShow==false) {
-			return;
+	private void init(Context context) {
+		mImageView = new ImageView(context);
+		
+		mTextView = new TextView(context);
+		if(mTextView != null) {
+			mTextView.setTextColor(0);
+			mTextView.setTextSize(12);
+			mTextView.setTypeface(null, Typeface.BOLD);
+			mTextView.setGravity(Gravity.CENTER);
 		}
-		if (subapi == null)
-		{
-			Log.i(LOG_TAG,	"subapi!!!!!!!!!!!!!!!  null"  );
-			return;
+		wscale = 1.000f;
+		hscale = 1.000f;
+		
+		SubManager.getinstance();
+	}
+	
+	public void setTextColor(int color) {
+		if(mTextView != null) {
+			mTextView.setTextColor(color);
 		}
-		int modifytime =millisec+timeoffset;
-		if(data!=null)
-		{
-			Log.i(LOG_TAG,	"modifytime :"+modifytime  +"exist b:"+data.beginTime()+" e:"+data.endTime() );
-
-			if(modifytime>=data.beginTime()&& modifytime<=data.endTime())
-			{
-				return ;
-			}
-		}	
-			
-		data = subapi.getdata(modifytime);
-		if(data == null)
-		{
-			Log.i(LOG_TAG,	"SubData return  null,so clear the view content"  );
-			setText("");
-			inter_bitmap=null;
-			invalidate(); 
+	}
+	
+	public void setTextSize(int size) {
+		if(mTextView != null) {
+			mTextView.setTextSize(size);
 		}
-		else
-		{
-			if(data.gettype()==1)
-			{
-				Log.i(LOG_TAG,"start gettype()==1 bitmap");		
-				inter_bitmap=data.getSubBitmap();
-				if(inter_bitmap!=null)
-				{
-//					Log.i(LOG_TAG,	"window" +this.getWidth()+"X"+this.getHeight() );
-//					Log.i(LOG_TAG,	"invalidate " +inter_bitmap.getWidth()+"X"+inter_bitmap.getHeight() );
-					//setLayoutParams(new LinearLayout.LayoutParams(inter_bitmap.getWidth(),inter_bitmap.getHeight()));
-//					Log.i(LOG_TAG,	"window" +this.getWidth()+"X"+this.getHeight() );
-			        invalidate(); 
+	}
+	
+	public void setTextStyle(int style) {
+		if(mTextView != null) {
+			mTextView.setTypeface(null, style);
+		}
+	}
+	
+	public void setGravity(int gravity) {
+		if(mTextView != null) {
+			mTextView.setGravity(gravity);
+		}
+	}
+	
+	public void setViewStatus(boolean flag) {
+		needSubTitleShow = flag;
+		if(flag == false) {
+			setVisibility(INVISIBLE); 
+		}
+		else {
+			setVisibility(VISIBLE); 
+		}
+	}
+	
+	public void clear() {
+		data = null;
+		SubManager.getinstance().clear();
+		wscale = 1.000f;
+		hscale = 1.000f;
+		this.removeAllViews();
+		this.requestLayout();
+	}
+	
+	public void redraw() {
+		this.removeAllViews();
+		if(data != null) {
+			if(data.gettype() == 1) {	
+				evaluteScale();
+				Bitmap inter_bitmap = creatBitmapByScale(data.getSubBitmap(), 
+						wscale, hscale);
+				if((inter_bitmap != null) && (mTextView != null)) {
+					mImageView.setImageBitmap(inter_bitmap);
+					this.addView(mImageView);
 				}
-				return;
-			}else	
-			{
+			}
+			else {
 				String sttmp = data.getSubString();		
 				sttmp=sttmp.replaceAll("\r","");
 				byte sttmp_2[] = sttmp.getBytes();
 				if( sttmp_2.length > 0 && 0 == sttmp_2[ sttmp_2.length-1] )
 					sttmp_2[ sttmp_2.length-1] = 0x20;
-				setText( new String( sttmp_2 ) );
+				if(mTextView != null) {
+					mTextView.setText( new String( sttmp_2 ) );
+					this.addView(mTextView);
+				}
 		    }
 		}
+		this.requestLayout();
+	}
+
+	public void setDisplayResolution(int width, int height) {
+		SubManager.getinstance().setDisplayResolution(width, height);
 	}
 	
-	public void setDelay(int milsec) 
-	{
-		timeoffset=milsec;
+	public void setVideoResolution(int width, int height) {
+		SubManager.getinstance().setVideoResolution(width, height);
+	}
+
+	public void evaluteScale() {
+		float w_scale = 1.000f;
+		float h_scale = 1.000f;
+		int display_width = 0;
+		int display_height = 0;
+		int video_width = 0;
+		int video_height = 0;
+
+		wscale = 1.000f;
+		hscale = 1.000f;
+		
+		display_width = SubManager.getinstance().getDisplayWidth();
+		display_height = SubManager.getinstance().getDisplayHeight();
+		video_width = SubManager.getinstance().getVideoWidth();
+		video_height = SubManager.getinstance().getVideoHeight();
+		
+		Log.d(TAG, "disply width: " + display_width + ", height: " + display_height);
+		Log.d(TAG, "video width: " + video_width + ", height: " + video_height);
+		
+		if((display_width <= 0) || (display_height <= 0) 
+				|| (video_width <= 0) || (video_height <= 0)) {
+			return;
+		}
+
+		if((video_width <= display_width) && (video_height <= display_height)) {
+			return; 
+		}
+		
+		if(this.getWidth() == display_width) {
+			w_scale = ((float)display_width)/video_width;
+			h_scale = ((float)display_height)/video_height;
+		}
+		else if(this.getWidth() == display_height){
+			w_scale = ((float)display_height)/video_width;
+			h_scale = ((float)display_width)/video_height;
+		}
+		
+		//Log.d(TAG, "w_scale: " + Float.toString(w_scale));
+		//Log.d(TAG, "h_scale: " + Float.toString(h_scale));
+		
+		if((w_scale < 0.000f) || (h_scale < 0.000f)) {
+			return;
+		}
+		
+		wscale = w_scale;
+		hscale = h_scale;
+	}
+	
+	public static Bitmap creatBitmapByScale(Bitmap bitmap, float w_scale, float h_scale) {   
+		if( bitmap == null ) {   
+			return null;   
+		}  
+		
+		int w = bitmap.getWidth();
+		int h = bitmap.getHeight(); 
+		/*
+		Log.d(TAG, "bitmap width: " + w + ", height: " + h 
+				+ ", w_scale: " + Float.toString(w_scale)
+				+ ", h_scale: " + Float.toString(h_scale));
+		*/
+		Matrix matrix = new Matrix();
+		matrix.postScale(w_scale, h_scale); 
+		Bitmap resizedBitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, matrix, true );   
+		return resizedBitmap;   
+	}
+	
+	public void tick(int millisec) {
+		if(needSubTitleShow == false) {
+			return;
+		}
+
+		int modifytime = millisec + timeoffset;
+		if(data != null) {
+			if((modifytime >= data.beginTime()) && (modifytime <= data.endTime())) {
+				if(getVisibility() == View.GONE)
+					return ;
+			}
+			else {
+				data = SubManager.getinstance().getSubData(modifytime);
+			}
+		}
+		else {
+			data = SubManager.getinstance().getSubData(modifytime);
+		}
+		
+		redraw();
+	}
+	
+	public void setDelay(int milsec) {
+		timeoffset = milsec;
 	}
 	
 	@Override
-	public void onDraw(Canvas canvas) 
-	{
-		 /*
-		 if(mbmpTest!=null)
-		 {
-		     Rect rtSource = new Rect(0,0,320,240);
-		     Rect rtDst = new Rect(0,0,320,240);
-		     canvas.drawBitmap(mbmpTest, rtSource,rtDst, mPaint);
-		 }
-		*/
-		
-		 if(inter_bitmap!=null)
-		 {
-		   Matrix matrix = new Matrix();
-           matrix.postScale(1.0f, 1.0f);
-           //matrix.setRotate(90,120,120);
-           int offset_x =0;
-           int offset_y =0;
-           int display_width=0;
-           int display_height=0;
-           
-           
-//           Log.i(LOG_TAG,"...window.......bitmap......." +this.getWidth()+"X"+this.getHeight() +"  " +inter_bitmap.getWidth()+"X"+inter_bitmap.getHeight() );
-   	   	  if(graphicViewMode==0)
-   	   	  {  
-   	   		  if(inter_bitmap.getHeight()<=this.getHeight()&&inter_bitmap.getWidth()<=this.getWidth())
-   	   		  {
-  				 display_width = inter_bitmap.getWidth();
-	       	     display_height =inter_bitmap.getHeight();
-	       	     offset_x = (this.getWidth()-display_width)/2;
-	       	     offset_y = this.getHeight()-display_height;  
-   	   		  }else
-   	   		  {
-   				if((((float)this.getWidth())/inter_bitmap.getWidth())>(((float)this.getHeight())/inter_bitmap.getHeight()))
-   				{
-   					display_width = inter_bitmap.getWidth()*this.getHeight()/inter_bitmap.getHeight();
-   		       	    display_height = this.getHeight();		
-   		       	    offset_x = (this.getWidth()-display_width)/2;
-   		       	    offset_y = 0;
-   				}else 
-   				{
-   					display_width = this.getWidth();
-   		       	    display_height =inter_bitmap.getHeight()*this.getWidth()/inter_bitmap.getWidth();
-   		       	    offset_x = 0;
-   		       	    offset_y = this.getHeight()-display_height;        	    		
-   				}		  
-   	   		  }
-   	   	  }else
-   	   	  {
-			if((((float)this.getWidth())/inter_bitmap.getWidth())>(((float)this.getHeight())/inter_bitmap.getHeight()))
-			{
-				display_width = inter_bitmap.getWidth()*this.getHeight()/inter_bitmap.getHeight();
-	       	    display_height = this.getHeight();		
-	       	    offset_x = (this.getWidth()-display_width)/2;
-	       	    offset_y = 0;
-			}else 
-			{
-				display_width = this.getWidth();
-	       	    display_height =inter_bitmap.getHeight()*this.getWidth()/inter_bitmap.getWidth();
-	       	    offset_x = 0;
-	       	    offset_y = this.getHeight()-display_height;        	    		
-			}
-   	   	  }
-			
-//test mode 
-//   	   	  {
-//				display_width = inter_bitmap.getWidth();
-//	       	    display_height =inter_bitmap.getHeight();
-//	       	    offset_x = (this.getWidth()-display_width)/2;
-//	       	    offset_y = this.getHeight()-display_height;    
-//   	   	  }
-			
-//			Log.i(LOG_TAG, "....x y w h.........."+offset_x+" "+offset_y+" "+display_width+" "+display_height );
-
-           
-           Rect Src = new Rect(0,0,inter_bitmap.getWidth(),inter_bitmap.getHeight());
-		   Rect Dst = new Rect(offset_x,offset_y,offset_x+display_width,offset_y+display_height);
-           canvas.drawBitmap(inter_bitmap,Src,Dst,null);
-   
-
-           
-//           Log.i(LOG_TAG,"end draw bitmap ");
-           //inter_bitmap.recycle();
-           inter_bitmap = null;
-		 } 	
-		 super.onDraw(canvas); 
-
+	public void onDraw(Canvas canvas) {
+		super.onDraw(canvas); 
     }
     
-    public void closeSubtitle()
-    {
-		if(subapi!=null)
-		{
-			Log.i("SubView", "------------release subtitle-----------" );
-			subapi.closeSubtitle();
-			subapi = null;				
-		}    	
-		if(hasopenInsubfile==true)
-		{
-			hasopenInsubfile=false;
-			subtitle.setSubname("INSUB"); 
-			try {
-				subapi=subtitle.parse();
-				subapi.closeSubtitle();
-				subapi=null;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+    public void closeSubtitle() {
+    	SubManager.getinstance().closeSubtitle();
     }
 
 	public Subtitle.SUBTYPE setFile(SubID file, String enc) throws Exception {
-
-		Log.i("SubView", "------------setFile-----------" +file.filename+"encoding:"+enc);
-
-		if(subapi!=null)
-		{
-			if(subapi.type()==Subtitle.SUBTYPE.INSUB)
-			{
-				//don't release now,apk will call closeSubtitle when change file.
-			}else
-			{
-				subapi.closeSubtitle();
-				subapi = null;
-			}
-		}
-		subtitle.setSystemCharset(enc);
-		InsubStatus=false;
-		// load Input File
-		try {
-			subtitle.setSubID(file);
-		    type=subtitle.getSubType();
-		    if (type==Subtitle.SUBTYPE.SUB_INVALID) 
-		    {
-		    	subapi =null;
-		    }
-		    else
-		    {
-		    	if(type==Subtitle.SUBTYPE.INSUB)
-		    	{
-		    		hasopenInsubfile =true;
-		    	}
-		    	subapi =subtitle.parse();
-	    	}
-		Log.i("SubView", "--subapi=----------------" +subapi);
-		} catch (Exception e) {
-		    Log.i("SubView", "------------err-----------" );
-			throw e;
-		}
-
-		return type;
+		return SubManager.getinstance().setFile(file, enc);
 	}
 
 	public SubtitleApi getSubtitleFile() {
-		return subapi;
+		return SubManager.getinstance().getSubtitleFile();
 	}
-
-//	public void reSet() {
-//		setText("");
-//		setCompoundDrawablesWithIntrinsicBounds(null, null, null,null); 
-//		if (subapi != null) {
-//			subapi.setCurSubtitleIndex(0);
-//		}
-//	}
-//
-//	public void showPrevSubtitle() {
-//		if (subFile == null||InsubStatus==false) {
-//			return;
-//		}
-//
-//		subFile.toPrevSubtitle();
-//
-//		setText(subFile.curSubtitle().getText());
-//	}
-//
-//	public void showNextSubtitle() {
-//		if (subFile == null||InsubStatus==false) {
-//			return;
-//		}
-//
-//		subFile.toNextSubtitle();
-//
-//		setText(subFile.curSubtitle().getText());
-//	}
 }
