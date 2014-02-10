6699089aec5e79a6dd6ba5e88fb27de0b7ca2cf7
Lujian.hu <Lujian.hu@amlogic.com>
2014-02-10 15:48:39 +0800

    PD #86868: use syswrite to get/set sub.

--- jni/subtitle/sub_set_sys.c
+++ jni/subtitle/sub_set_sys.c
@@ -10,308 +10,79 @@
 #include <log_print.h>
 #include "linux/ioctl.h"
 #include "amstream.h"
+#include <Amsysfsutils.h>
 
 #define CODEC_AMSUBTITLE_DEVICE     "/dev/amsubtitle"
 
-typedef enum {
-    SUB_NULL = -1,
-    SUB_ENABLE = 0,
-    SUB_TOTAL,
-    SUB_WIDTH,
-    SUB_HEIGHT,
-    SUB_TYPE,
-    SUB_CURRENT,
-    SUB_INDEX,
-    SUB_WRITE_POS,
-    SUB_START_PTS,
-    SUB_FPS,
-    SUB_SUBTYPE,
-    SUB_RESET,
-    SUB_DATA_T_SIZE,
-    SUB_DATA_T_DATA
-}subinfo_para_type;
-
-typedef struct {
-    subinfo_para_type subinfo_type;
-    int subtitle_info;
-    char *data;
-} subinfo_para_t;
-
 #define msleep(n)	usleep(n*1000)
 
-int codec_h_open(const char *port_addr, int flags)
+int set_sysfs_int(const char *path, int val)
 {
-    int r;
-	int retry_open_times=0;
-retry_open:
-    r = open(port_addr, flags);
-    if (r<0 /*&& r==EBUSY*/) {
-	    retry_open_times++;
-	    if(retry_open_times==1)
-          log_print("Init [%s] failed,ret = %d retry_open!\n", port_addr, r);
-	    msleep(10);
-	    if(retry_open_times<1000)
-	       goto retry_open;
-	    log_print("retry_open [%s] failed,ret = %d used_times=%d*10(ms)\n", port_addr,r,retry_open_times);
-		
-        return r;
-    }
-	if(retry_open_times>0)
-		log_print("retry_open [%s] success,ret = %d used_times=%d*10(ms)\n", port_addr,r,retry_open_times);
-    return r;
+    return amsysfs_set_sysfs_int(path, val);
 }
-
-int codec_h_close(int h)
+int get_sysfs_int(const char *path)
 {
-	int r;
-    if (h >= 0) {
-        r = close(h);
-		if (r < 0) {
-        	log_print("close failed,handle=%d,ret=%d \n", h, r);
-    	}
-    }
-    return 0;
-}
-
-
-int set_subtitle_info(subinfo_para_t sub_info)
-{
-    int utils_fd, ret;
-
-    utils_fd = codec_h_open(CODEC_AMSUBTITLE_DEVICE, O_RDWR);
-    if (utils_fd < 0) {
-        log_print("[%s::%d] open_amsubtitle failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-	
-    ret = ioctl(utils_fd, AMSTREAM_IOC_SET_SUBTITLE_INFO, (unsigned long)&sub_info);
-    if (ret < 0) {
-        log_print("## amsubtitle set failed -------\n");
-        codec_h_close(utils_fd);
-        return -1;
-    }
-
-    codec_h_close(utils_fd);
-
-	return 0;
-}
-
-int get_subtitle_info(subinfo_para_t *sub_info)
-{
-    int utils_fd, ret;
-
-    utils_fd = codec_h_open(CODEC_AMSUBTITLE_DEVICE, O_RDWR);
-    if (utils_fd < 0) {
-        log_print("[%s::%d] open_amsubtitle failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-	
-    ret = ioctl(utils_fd, AMSTREAM_IOC_GET_SUBTITLE_INFO, (unsigned long)sub_info);
-    if (ret < 0) {
-        log_print("## amsubtitle set failed -------\n");
-        codec_h_close(utils_fd);
-        return -1;
-    }
-
-    codec_h_close(utils_fd);
-
-	return 0;
+    return amsysfs_get_sysfs_int16(path);
 }
 
 int set_subtitle_enable(int enable)
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_ENABLE;
-    sub_info.subtitle_info = enable;
-    log_print("## set_subtitle_enable %d, ----\n", enable);
-
-    ret = set_subtitle_info(sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return 0;
+	log_print("[%s::%d] ----- \n",__FUNCTION__,__LINE__);
+    return set_sysfs_int("/sys/class/subtitle/enable", enable);
 }
 
 int get_subtitle_enable()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_ENABLE;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+	log_print("[%s::%d] ------ \n",__FUNCTION__,__LINE__);
+    return get_sysfs_int("/sys/class/subtitle/enable");
 }
 
 int get_subtitle_num()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_TOTAL;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/total");
 }
 
 int set_subtitle_curr(int curr)
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_CURRENT;
-    sub_info.subtitle_info = curr;
-
-    ret = set_subtitle_info(sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return 0;
+    return set_sysfs_int("/sys/class/subtitle/curr", curr);
 }
 
 int get_subtitle_curr()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_CURRENT;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/curr");
 }
 
 int set_subtitle_size(int size)
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_DATA_T_SIZE;
-    sub_info.subtitle_info = size;
-
-    ret = set_subtitle_info(sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return 0;
+    return set_sysfs_int("/sys/class/subtitle/size", size);
 }
 
 int get_subtitle_size()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_DATA_T_SIZE;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/size");
 }
 
 int set_subtitle_data(int data)
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_DATA_T_DATA;
-    sub_info.subtitle_info = data;
-
-    ret = set_subtitle_info(sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return 0;
+    return set_sysfs_int("/sys/class/subtitle/data", data);
 }
 
 int get_subtitle_data()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_DATA_T_DATA;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/data");
 }
 
 int get_subtitle_startpts()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_START_PTS;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/startpts");
 }
 
 int get_subtitle_fps()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_FPS;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/fps");
 }
 
 int get_subtitle_subtype()
 {
-    subinfo_para_t sub_info;
-    int ret;
-
-    sub_info.subinfo_type = SUB_SUBTYPE;
-
-    ret = get_subtitle_info(&sub_info);
-    if (ret<0) {
-        log_print("[%s::%d] set_subtitle_num failed! \n",__FUNCTION__,__LINE__);
-        return -1;
-    }
-
-    return sub_info.subtitle_info;
+    return get_sysfs_int("/sys/class/subtitle/subtype");
 }
-
